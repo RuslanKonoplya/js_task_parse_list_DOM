0526874af9217d2eb23a620a5560cb478da8a63b
Ruslan <ruslan.konoplya@gmail.com>
2024-12-16 14:39:03 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // write code here
+const getList = document.querySelectorAll('li');
+const arr = Array.from(getList);
+
+function sortList(list) {
+  const sorted = list.sort((a, b) => {
+    const salaryA = parseFloat(
+      a.getAttribute('data-salary').replace(/[^0-9.-]+/g, ''),
+    );
+    const salaryB = parseFloat(
+      b.getAttribute('data-salary').replace(/[^0-9.-]+/g, ''),
+    );
+
+    return salaryA - salaryB;
+  });
+  const removeUl = document.querySelector('ul');
+
+  removeUl.innerHTML = '';
+  sorted.forEach((item) => removeUl.prepend(item));
+
+  return sorted;
+}
+
+sortList(arr);
