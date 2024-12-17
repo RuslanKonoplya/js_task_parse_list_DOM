4b685a2e0f08ff3e82ee7a823f91700913315ee3
Ruslan <ruslan.konoplya@gmail.com>
2024-12-17 11:50:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,14 +13,28 @@ function sortList(list) {
       b.getAttribute('data-salary').replace(/[^0-9.-]+/g, ''),
     );
 
-    return salaryA - salaryB;
+    return salaryB - salaryA;
   });
   const removeUl = document.querySelector('ul');
 
   removeUl.innerHTML = '';
-  sorted.forEach((item) => removeUl.prepend(item));
+  sorted.forEach((item) => removeUl.append(item));
 
   return sorted;
 }
 
+function getEmployees(list) {
+  const result = list.map((elem) => {
+    return {
+      name: elem.innerText,
+      position: elem.getAttribute('data-position'),
+      salary: elem.getAttribute('data-salary'),
+      age: elem.getAttribute('data-age'),
+    };
+  });
+
+  return result;
+}
+
 sortList(arr);
+getEmployees(arr);
